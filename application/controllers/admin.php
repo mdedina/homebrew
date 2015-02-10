0613a01e3bc6397a8804e96773c8c4c05a87cb55
Michael Dedina <mdedina@gmail.com>
2015-02-10 14:39:12 -0800

    on login set session variables is_valid and admin_id

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -5,7 +5,7 @@ class Admin extends CI_Controller {
 	public function __construct()
 	{
 		parent::__construct();
-		// $this->output->enable_profiler();
+		$this->output->enable_profiler();
 		$this->load->model("homebrew");
 	}
 
@@ -13,33 +13,44 @@ class Admin extends CI_Controller {
 		$this->load->view('adminviews/admin');
 	}
 
+	/*
+	 * After login:
+	 * 	'is_valid' will be TRUE if the input data was valid, else FALSE.
+	 *  'admin_id' will contain the user id if logon was successful, else empty.
+	 */
 	public function login()
 	{
-		// var_dump($this->input->post());
-		$messages = $this->validate_login($this->input->post());
-		if (!empty($messages)) {
-			//TODO load a partial to display invalid input messages
-			return;
+		$this->session->set_userdata('admin_id', "");
+		$this->session->set_userdata('is_valid', $this->validate_login($this->input->post()));
+
+		if ($this->session->userdata('is_valid')) {
+			$this->session->set_userdata('admin_id', $this->homebrew->admin_login($this->input->post()));
 		}
 
-		$admin_id = $this->homebrew->admin_login($this->input->post());
-		if (!$admin_id) {
-			//TODO load a partial to indicate user not found
-			echo "<p>login failed</p>";
-			return;
+		if (empty($this->session->userdata('admin_id'))) {
+			redirect('/admin');
 		}
 
-		$this->session->set_userdata('admin_id', $admin_id);
-
-		$products = $this->homebrew->get_all_products();
-
-		$this->load->view("adminviews/products", array("products", $products));
-
+		redirect('/admin/products');
 	}
 
+	public function products() {
+		$products = $this->homebrew->get_all_products();
+		$this->load->view("adminviews/products", array("products", $products));
+	}
+
+	// should be called by the 'add new product' button
+	public function add_product() {
+		$this->load->view("adminviews/add.php");
+	}
+
+
+	// is the login data valid?
 	private function validate_login($post) {
-		//TODO check user and pw are not empty
-		return "";
+		if (empty($post['adminemail']) || empty($post['adminpassword'])) {
+			return FALSE;
+		}
+		return TRUE;
 	}
 
 
